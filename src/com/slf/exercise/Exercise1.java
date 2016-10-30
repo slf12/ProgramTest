2894c8b9bc43c70ef1e2960eb77c3ac9c44b4037
slf12 <slf12thuss@163.com>
2016-10-30 14:00:15 +0800

    练习1

---
+++ src/com/slf/exercise/Exercise1.java
@@ -0,0 +1,19 @@
+package com.slf.exercise;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("please input n (n >= 1)");
+		Scanner scr = new Scanner(System.in);
+		Long input = scr.nextLong();
+		for(int i = 1; i <= input; i++){
+			for(int j = 0; j <= i; j++){
+				
+			}
+		}
+	}
+
+}
--- src/com/slf/math/BigInteger.java
+++ src/com/slf/math/BigInteger.java
@@ -1,10 +1,62 @@
 package com.slf.math;
 
 public class BigInteger {
-
+	
+	public static String bigIntegerAdd(String s1, String s2){
+		if (s1.isEmpty() || s2.isEmpty()) {
+			return null;
+		} else if (s1.equals("0")) {
+			return s2;
+		} else if (s2.equals("0")) {
+			return s1;			
+		}
+		
+		if (s1.startsWith("-")) {
+			if (s2.startsWith("-")) {
+				return "-" + bigIntegerAdd(s1.substring(1), s2.substring(1));			
+			} else {
+				return bigIntegerSub(s2, s1);
+			}
+		} else {
+			if (s2.startsWith("-")) {
+				return bigIntegerSub(s1, s2);
+			}
+		}
+		
+		int size1 = s1.length();
+		int size2 = s2.length();
+		if (size1 < size2) {
+			for(int i = 0; i < size2 - size1; i++){
+				s1 = "0" + s1;
+			}
+		} else {
+			for(int i = 0; i < size1 - size2; i++){
+				s2 = "0" + s2;
+			}
+		}
+		int p = 0;
+		int q = 0;
+		String result = "";
+		for(int i = s1.length() - 1; i >=0; i--){
+			p = (s1.charAt(i) - '0' + s2.charAt(i) - '0' + q) % 10;
+			q = (s1.charAt(i) - '0' + s2.charAt(i) - '0' + q) / 10;
+			result = (char)( p + '0') + result;
+		}
+		
+		if (q == 1) {
+			result = "1" + result;
+		}
+		
+		return result;
+	}
+	
+	public static String bigIntegerSub(String s1, String s2){
+		
+		return "";
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(BigInteger.bigIntegerAdd("-127899999999", "-123"));
 	}
 
 }
